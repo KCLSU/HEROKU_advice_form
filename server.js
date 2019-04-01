49961b4d5bc703b9bde5449b7339122c835375bc
Kyle Robinson <kylerobbo12@gmail.com>
2019-04-01 19:05:52 +0100

    post bug

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var app = express();
 var PORT = process.env.PORT || 3000
 
 
-app.get('/', (req, res) => {
-  res.send("SUCCESS");
-})
+// app.get('/', (req, res) => {
+//   res.send("SUCCESS");
+// })
 
-app.post('/', (req, res) => {
+app.get('/:stri', (req, res) => {
   let testString = req.params.stri;
   res.status(200).json({testString: testString});
 })
