3df88c3ff36dfb51ce1e588f427e9c1d9aa92c3a
Kyle Robinson <kylerobbo12@gmail.com>
2019-04-01 18:45:01 +0100

    remove get req

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ var express = require('express')
 var app = express();
 var PORT = process.env.PORT || 3000
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
 app.get('/', (req, res) => {
   res.send("SUCCESS");
