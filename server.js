fbd0986d56070c1d32de686702dcf3ddda730999
Kyle Robinson <kylerobbo12@gmail.com>
2019-04-02 10:36:20 +0100

    slide back

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ app.get('/', (req, res) => {
   res.send("SUCCESS");
 })
 
-app.get('/test/?stri', (req, res) => {
-  let testString = req.params.stri;
-  res.status(200).json({testString: testString});
-}).catch(er => res.send(er))
+// app.get('/test/?stri', (req, res) => {
+//   let testString = req.params.stri;
+//   res.status(200).json({testString: testString});
+// }).catch(er => res.send(er))
 
 app.listen(PORT, () => {
   console.log("The server is running and listening on port " + PORT)
