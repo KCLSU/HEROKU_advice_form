cdc2f6da58dadeb8b3fdac438bf12050e7054680
Kyle Robinson <kylerobbo12@gmail.com>
2019-04-02 10:24:18 +0100

    add catch error

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ var PORT = process.env.PORT || 3000
 //   res.send("SUCCESS");
 // })
 
-app.get('/:stri', (req, res) => {
+app.get('/test/?stri', (req, res) => {
   let testString = req.params.stri;
   console.log(testString)
   res.status(200).json({testString: testString});
-})
+}).catch(er => res.send(er))
 
 app.listen(PORT, () => {
   console.log("The server is running and listening on port " + PORT)
