867741c9d732b9932786c9c61f0417223dd5e852
Kyle Robinson <kylerobbo12@gmail.com>
2019-04-03 16:52:14 +0100

    enable CORS

--- sendData.js
+++ sendData.js
@@ -4,16 +4,31 @@ var url = 'http://localhost:2000/server'
 
 
 function sendData(str){
+
+  console.log("Received string")
+  console.log(str)
   let postData = {};
       postData.method = 'POST';
-      postData.body = JSON.stringify({str});
+      postData.body = JSON.stringify(str);
       postData.headers = {
       'Content-Type': 'application/json'
     }
 
+  console.log("Stringified string")
+  console.log(postData.body)
+  console.log(typeof postData.body)
+
   return fetch(url, postData)
-            .then(response => response.json())
-            .catch(err => console.log(err))
+            .then(response => {
+              console.log("Send Data")
+               console.log(response.json())
+              console.log(response.ok)
+              return "Submitted";
+            })
+            .catch(err => {
+              console.log(err)
+              return "Failed"
+            })
 }
 
 module.exports = sendData;
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express')
+var bodyParser = require('body-parser');
 var sendData = require('./sendData.js')
+var util = require('util');
 var app = express();
+var cors = require('cors');
 var PORT = process.env.PORT || 3000
+
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(cors());
 
 
 app.get('/', (req, res) => {
@@ -11,9 +19,22 @@ app.get('/', (req, res) => {
 app.get('/entry/:str', (req, res) => {
   let str = req.params.str;
   sendData(str)
-    .then(outcome => res.status(202).send(outcome))
-    .catch(err => res.status(500).send(err))
+  .then(outcome => res.status(202).send(outcome))
+  .catch(err => {res.status(500).send({"error":err, "status": "Failed"})})
 });
+
+app.post('/send', (req, res) => {
+  let data = req.body;
+  console.log(typeof data)
+  console.log("HEROKU data")
+  console.log(data)
+    console.log("UTIL INSPECT")
+   console.log(util.inspect(data));
+  sendData(data)
+    .then(outcome => res.status(202).send(outcome))
+    .catch(err => res.status(500).send({"error":err, "status": "Failed"}))
+});
+
 
 app.listen(PORT, () => {
   console.log("The server is running and listening on port " + PORT)
