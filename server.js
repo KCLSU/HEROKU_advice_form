6f5a75d25f0439a7d24e24222cecc27d87bd7207
KCLSU <45687039+KCLSU@users.noreply.github.com>
2020-04-23 17:14:00 +0200

    add firebase areas auth

--- server.js
+++ server.js
@@ -47,8 +47,8 @@ app.post('/send', (req, res) => {
     .catch(err => res.status(500).send({"error":err, "status": "Failed"}))
 });
 
-app.post('/authenticate', (req, res) => {
-    firebaseAuth(req.body)
+app.post('/authenticate', (req, res) => {  
+    firebaseAuth(req.body.package, req.body.area)
     .then(data => res.status(200).send(data))
     .catch(err => res.status(400).send(err))
 });
