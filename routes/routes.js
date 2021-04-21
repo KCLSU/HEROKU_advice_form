a99363fe85c4ba9ec8e8a662f0d197d9aa9ea8ed
Kyle Robinson <kylerobbo12@gmail.com>
2021-04-21 08:06:25 +0200

    advice pro middleware

--- routes/routes.js
+++ routes/routes.js
@@ -16,12 +16,11 @@ router.post('/submitAdvicePro',
     validate, 
     controllers.advicepro.submitToAdvicePro);
 
-router.post('/authenticate', 
-    controllers.auth.authenticate);
-router.post('/upload_image',
-    controllers.cloudinary.upload);
-router.post('/transform', 
-    controllers.cloudinary.transform);
+router.post('/authenticate',controllers.auth.authenticate);
+
+router.post('/upload_image', controllers.cloudinary.upload);
+
+router.post('/transform', controllers.cloudinary.transform);
 
 
 module.exports = router;
