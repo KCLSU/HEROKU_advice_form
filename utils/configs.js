28c74c6a668b1550345f435d125d6dec6844b2d1
Kyle Robinson <kylerobbo12@gmail.com>
2021-06-18 17:35:44 +0200

    add in remaining configs

--- utils/configs.js
+++ utils/configs.js
@@ -24,6 +24,10 @@ exports.firebaseServiceAccount = () => {
     "project_id": stringVals.FIREBASE_PROJECT_ID,
     "private_key": stringVals.FIREBASE_PRIVATE_KEY,
     "client_email": stringVals.FIREBASE_CLIENT_EMAIL,
+    "auth_uri": stringVals.FIREBASE_AUTH_URI,
+    "token_uri": stringVals.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": stringVals.FIREBASE_AUTH_PROVIDER,
+    "client_x509_cert_url": stringVals.FIREBASE_CLIENT_X509_CERT_URL
   } 
 }
 
