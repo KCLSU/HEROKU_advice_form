8906a9c9851678b2b680de0a5b55fe76662da2d2
Kyle Robinson <kylerobbo12@gmail.com>
2021-06-22 16:52:28 +0200

    check for valid user and referer before iterating

--- middlewares/approveUser.js
+++ middlewares/approveUser.js
@@ -6,12 +6,12 @@ exports.approveUser = (req, res, next) => {
     //Check if IP has had a token issued previously. If not, check referer and protocol
     //Could add an IP location based check here too
     let validIp = req.serverTokens.find(item => item.ip === userIp);
-    let validUser = validIp || 
-        req.get('origin').includes('kclsu.org')
-        && req.get('referer').includes('manager')
-        // && req.secure;
+    let validOrigin = req.get('origin') && req.get('origin').includes('kclsu.org');
+    let validReferer = req.get('referer') && req.get('referer').includes('manager');
+    let validUser = validIp || validOrigin && validReferer;
+    // && req.secure;
     if (!validUser){
-        res.status(400).send(errorResponse('The user in invalid'));
+        res.status(400).send(errorResponse('The user is invalid'));
         //UPDATE DATABASE ERROR LOG
         logError('Auth', 'User not valid',  req )
     }
